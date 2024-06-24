289804b7ef8e8fb6d54af01d6afa5f71896f680f
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-06-24 18:14:19 +0530

    add :  wait after deploy in testing

--- test/Box.js
+++ test/Box.js
@@ -8,6 +8,12 @@ describe("Box contract testing V1", function () {
   beforeEach(async function(){
     Box = await ethers.getContractFactory("Box");
     box = await Box.deploy();
+    await box.waitForDeployment();
+
+
+    //console.log("RoboPunksNFT deployed to:", roboPunksNFT.address); changed to
+    //console.log("RoboPunksNFT deployed to:",await roboPunksNFT.getAddress());
+
   });
   it("retrieve function returns a value previously stored",async function(){
     await box.store(10);
