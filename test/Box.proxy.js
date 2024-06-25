da384bfc6f6975321f0a925929e62dad1a355390
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-06-25 08:17:47 +0530

    add : proxyContract test pass

---
+++ test/Box.proxy.js
@@ -0,0 +1,17 @@
+const {expect}=require('chai')
+const {ethers,upgrades}=require('hardhat')
+let Box;
+let box;
+
+describe("text box proxy functionality",function(){
+    beforeEach(async function(){
+        Box = await ethers.getContractFactory("Box");
+        //proxy contract create function and value
+        box  = await upgrades.deployProxy(Box,[10],{initializer:"store"})
+    });
+
+    //test case
+    it("retrive returns a value previosly initialized",async function(){
+        expect((await box.retrieve()).toString()).to.equal('10');
+    })
+})
